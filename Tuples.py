dfbdfe932e891166c80cbd8192caf178fc01de5a
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-08-13 21:48:37 +0300

    Tuples

---
+++ Tuples.py
@@ -0,0 +1,12 @@
+three_numbers = (17, 9, 23)
+print(three_numbers)
+
+strings = ('Royalty', 'Yukio')
+print(strings)
+
+mixed_value = (17, True, 'love')
+print(mixed_value)
+
+#tuple constructor
+test = tuple(('Today', 'i', 'am', 'happy'))
+print(test)
