a92a276fc9144fbfaa604e87fe2aae1303c07a45
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-08-16 07:48:26 +0300

    Dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,18 @@
+my_dict = {
+    'name': 'Yukio',
+    'nationality' : 'African'
+}
+print(my_dict) #Prints as object
+print(my_dict['name']) #Prints the value
+
+#Mixing data types
+my_dict = {
+    'name': 'Royalty',
+    'nationality' : 'African',
+    'age': 19,
+    'is_tall': True,
+    'friends': ['Tasha', 'Nancy', 'Yukio']
+}
+
+x = my_dict['friends']
+print(x)
